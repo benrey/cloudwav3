f5d62eb979710d557f0cb700fd3ba10cc521118b
benrey <ben.rey@gmail.com>
2022-02-06 13:29:45 -0600

    First send

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 20) {
+    $('nav').addClass('transparent');
+  } else {
+    $('nav').removeClass('transparent');
+  }
+});
+
+var icon = document.getElementsByClassName("icon-bars");
+var pushDown = document.getElementById("push");
+
+$(document).ready(function(){
+  $(icon[0]).click(function(){
+    if($(pushDown).hasClass("push")){
+      pushDown.className = "pushUp";
+    }
+    else{
+      pushDown.className = "push";
+    }
+  })
+});
